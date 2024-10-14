8870d0855545eeee96a776e652ad427fb6fcec3f
ArunShanmughan <arunshanmughan35@gmail.com>
2024-10-14 17:26:21 +0530

    inital commit

---
+++ Client/src/Components/Admin/AdmHome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AdminNavbar from './AdminNavbar'
+
+const AdmHome = () => {
+  return (
+    <div>
+      <AdminNavbar/>
+      
+    </div>
+  )
+}
+
+export default AdmHome
---
+++ Client/src/Components/Admin/AdminNavbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const AdminNavbar = () => {
+  return (
+    <div>
+      <>
+      <button>Users</button>
+      <button>Logout</button>
+      </>
+    </div>
+  )
+}
+
+export default AdminNavbar
